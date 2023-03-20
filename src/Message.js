dc0c214b60673e0fcd5f01b4609f4b3515fe647a
atefataya81 <atef.ataya@gmail.com>
2023-03-20 22:58:54 +0400

    color change

--- src/Message.js
+++ src/Message.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './Content.css';
-import Message from './Message';
 
-function Content(props) {
+function Message(props) {
   return (
     <div className={`content ${props.theme}`}>
-      <h1>Content</h1>
+      <h1>Message</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
         lobortis eros, quis consequat ipsum. Praesent elementum enim vitae augue
@@ -13,9 +12,8 @@ function Content(props) {
         accumsan aliquet. Praesent pulvinar consequat est, vel hendrerit nunc
         eleifend at.
       </p>
-      <Message theme={props.theme} />
     </div>
   );
 }
 
-export default Content;
+export default Message;
